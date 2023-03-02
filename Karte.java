308f03865fab4a664097ca4c38a57f58f52e7bf8
Dr. Peter Dillinger <peter.dillinger@srh.de>
2023-03-02 15:24:36 +0100

    Karte und Hauptprogramm implementiert!

--- Karte.java
+++ Karte.java
@@ -1,2 +1,34 @@
 public class Karte {
+    // Attribute
+    public int wert;
+    public String farbe;
+    public String format = "standard";
+
+    // Konstruktor
+    public /* kein Datentyp */ Karte() { //<- eher untypischer Konstruktor
+        System.out.println("Eine Karte wurde erzeugt!");
+    }
+
+    // klassicher Konstruktor, der alle Attribute initialisiert!
+    // (mit Hilfe der Parameter)
+    public Karte(int neuerWert, String neueFarbe, String neuesFormat) {
+        wert = neuerWert;
+        farbe = neueFarbe;
+        format = neuesFormat;
+    }
+
+    public Karte(int neuerWert, String neueFarbe) {
+        wert = neuerWert;
+        farbe = neueFarbe;
+    }
+
+
+    // Methoden
+    public void ziehen() {
+        System.out.println("Eine Karte wurde gezogen.");
+    }
+
+    public void ausspielen() {
+        System.out.println("Spiele " + farbe + wert);
+    }
 }
--- Main.java
+++ Main.java
@@ -1,5 +1,20 @@
 public class Main {
-    public static void main(String[] args) {
 
+    // Früher:
+    // ausspielen(Karte karte)
+    // Heute (OOP):
+    // karte.ausspielen();
+
+    public static void main(String[] args) {
+        Karte karte1 = new Karte(8, "grün");
+        Karte karte2 = new Karte(7, "rot");
+
+        karte1.ausspielen(); // <- typische Anwendung, Nutzung der Methoden
+        karte2.ziehen();
+        //System.out.println(karte1);
+
+        System.out.println(karte2.farbe);
+        karte2.wert = 8;                    // <- macht das Sinn? Ist es erlaubt???
+        karte2.ausspielen();
     }
 }
